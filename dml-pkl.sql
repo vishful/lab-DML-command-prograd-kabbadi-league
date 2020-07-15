ee145ed50e98020c234e2c8057b2a84e9eb8bfbf
Vishnu <vishnurpillai9914@gmail.com>
2020-07-15 18:10:41 +0530

    FSJ0520A101

--- dml-pkl.sql
+++ dml-pkl.sql
@@ -3,12 +3,12 @@
 
 --1. Insert into city
 INSERT INTO city(id,name)
-VALUES(01,"New York");
+VALUES(01,'New York');
 --2. Insert into referee
 INSERT INTO referee(id,name)
-VALUES(01,"Thomas");
+VALUES(01,'Thomas');
 INSERT INTO referee(id,name)
-VALUES(02,"Jessica");
+VALUES(02'Jessica');
 --3. Insert into innings
 INSERT INTO innings(id,innings_number)
 VALUES(6, 11);
@@ -19,20 +19,20 @@ INSERT INTO extra_type(id,event_id,extra_type_id,points,scoring_team_id)
 VALUES(2,2,2,4,2);
 --5. Insert into skill
 INSERT INTO skill(id,name)
-VALUES(1, "Bowling");
+VALUES(1, 'Bowling');
 --6. Insert into team
 INSERT INTO team(id,name,coach,home_city,captain)
-VALUES(1,"Wildcats","Thomas",1,1);
+VALUES(1,'Wildcats','Thomas',1,1);
 --7. Insert into player
 INSERT INTO player(id,name,country,skill_id,team_id)
-VALUES(1,"Liz","USA",1,1);
+VALUES(1,'Liz','USA',1,1);
 INSERT INTO player(id,name,country,skill_id,team_id)
-VALUES(2,"Stef","USA",1,2);
+VALUES(2,'Stef','USA',1,2);
 INSERT INTO player(id,name,country,skill_id,team_id)
-VALUES(3,"Marina","Wales",1,2);
+VALUES(3,'Marina','Wales',1,2);
 --8. Insert into venue
 INSERT INTO venue(id,stadium_name,city_id)
-VALUES(1,"SUPERBOWL",245);
+VALUES(1,'SUPERBOWL',245);
 --9. Insert into event
 INSERT INTO event(id,innings_id,ecent_no,raider_id,raid_points,defending_points,clock_in_seconds,team_one_seconds,team_two_score)
 VALUES(1,2345,123,134,43,2,3,456,765);
@@ -41,9 +41,9 @@ INSERT INTO extra_event(id,event_id,extra_type_id,points,scoring_team_id)
 VALUES(1,234,534,34,23);
 --11. Insert into outcome
 INSERT INTO outcome(id,status,winner_team_id,score,player_of_match)
-VALUES(1,"WINNER",123,6,5);
+VALUES(1,'WINNER',123,6,5);
 INSERT INTO outcome(id,status,winner_team_id,score,player_of_match)
-VALUES(2,"LOSER",124,3,3);
+VALUES(2,'LOSER',124,3,3);
 --12. Insert into game
 INSERT INTO game(id,game_date,team_id_1,team_id_2,venue_id,outcome_id,referee_id_1,referee_id_2,first_innings_id,second_innings_id)
 VALUES(1,"14-07-2020",1,2,3,4,5,234,456,142);
@@ -61,14 +61,14 @@ SET team_id=3
 where id=3;
 --16. Update player table
 UPDATE player
-SET country="UK"
+SET country='UK'
 where id=3;
 --17. Delete from extra_type
 DELETE from extra_type
 WHERE id=1;
 --18. Delete from referee
 DELETE from referee
-where name="Thomas";
+where name='Thomas';
 --19. Delete from player
 Delete from player
 where id=3;
